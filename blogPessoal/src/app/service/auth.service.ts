c303a055e7064c0e1efb50adbb325854fd9a2715
Padilha272 <matheuspadilha48@hotmail.com>
2021-12-07 17:17:18 -0300

    Componente inicio e consumo

--- blogPessoal/src/app/service/auth.service.ts
+++ blogPessoal/src/app/service/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { Usuario } from '../model/User';
 import { UserLogin } from '../model/UserLogin';
 
@@ -21,5 +22,14 @@ export class AuthService {
     return this.http.post<Usuario>('http://localhost:8080/usuarios/cadastrar', usuario)
   }
 
+  logado() {
+    let ok: boolean = false
+
+    if (environment.token !=""){
+        ok = true
+    }
+
+    return ok 
+  }
 
 }
