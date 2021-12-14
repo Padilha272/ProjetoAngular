4d54673cb9e6c20282ed0ffc231994e56ae2c4fc
Padilha272 <matheuspadilha48@hotmail.com>
2021-12-14 09:02:49 -0300

    implementation of put method

--- blogPessoal/src/app/service/postagem.service.ts
+++ blogPessoal/src/app/service/postagem.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
+import { PostagemDeleteComponent } from '../delete/postagem-delete/postagem-delete.component';
 import { Postagem } from '../model/Postagem';
 
 @Injectable({
@@ -19,8 +20,19 @@ export class PostagemService {
     return this.http.get<Postagem[]>('https://padiblog.herokuapp.com/postagens',this.token)
   }
 
+  getByIdPostagem(id: number): Observable<Postagem>{
+    return this.http.get<Postagem>(`https://padiblog.herokuapp.com/postagens/${id}`,this.token)
+  }
+
   postPostagem(postagem: Postagem): Observable<Postagem>{
     return this.http.post<Postagem>('https://padiblog.herokuapp.com/postagens',postagem,this.token)
   }
 
+  putPostagem(postagem:Postagem): Observable<Postagem>{
+    return this.http.put<Postagem>('https://padiblog.herokuapp.com/postagens',postagem,this.token)
+  }
+
+  deletePostagem(id: number){
+    return this.http.delete<Postagem>(`https://padiblog.herokuapp.com/postagens/${id}`,this.token)
+  }
 }
